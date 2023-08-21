f1a5b67b464772b95d687b0c8e99080efabd94e2
Sampriti Mukherjee <sampriti275@gmail.com>
2023-08-21 22:26:51 +0530

    day 3 LL

---
+++ src/LinkedList3/RotateList.java
@@ -0,0 +1,28 @@
+package LinkedList3;
+
+public class RotateList {
+//        public static Node rotate(Node head, int k) {
+//            // Write your code here.
+//            if(head == null && head.next == null) return head;
+//            Node temp = head;
+//            int length = 1;
+//            while(temp.next != null){
+//                temp = temp.next;
+//                length++;
+//            }
+//            if(k % length  == 0) return head;
+//            temp.next = head;
+//
+//            temp = head;
+//            int j =length -  (k % length);
+//            while(j > 1){
+//                temp = temp.next;
+//                j--;
+//            }
+//            Node start = temp.next;
+//            temp.next = null;
+//
+//            return start;
+//        }
+
+}
